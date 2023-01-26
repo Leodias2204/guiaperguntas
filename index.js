299df9e9dd44631285db3cf3e9541442f9dcf0d7
Leodias2204 <leonardodmonteiro@hotmail.com>
2023-01-25 21:10:08 -0300

    Introduction of body parser

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 
 
 app.set('view engine', 'ejs')// Estou dizendo para o express usar o EJS como o view engine
 app.use(express.static('public')) // use express static é utilizado para carregar imagens ou qualquer coisa estática para o HTML.
+
+
+//Body parser
+app.use(bodyParser.urlencoded({extended: false })) // Decodifica os dados do formulário em uma estrutura Java script utilizavel pelo back end.
+app.use(bodyParser.json()) 
+
+
+// Rotas
 app.get("/",(req,res) => {
     // Váriaveis criadas para serem vizualizadas diretamente no HTML.
 
     res.render("index",{
-
+        //Render é utilizado para rederizar o HTML
     })
 })
 
@@ -17,8 +26,16 @@ app.get("/perguntar",(req,res) => {
     res.render("perguntar")
 })
 
-app.listen(8080,() => {
-    console.log("App Rodando!!")
+
+
+//Tipo de rota utilizado para formulário
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send("Formulário recebido " + titulo + " descrição " + descricao)
 })
 
 
+app.listen(8080,() => {
+    console.log("App Rodando!!")
+})
